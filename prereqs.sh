f2961b0bba06a2ee6a77343f3059f09d44e5ded4
Mike Kelly <mikerkelly87@gmail.com>
2020-03-02 11:46:46 -0600

    Changed to use python2 per Bjoern

--- prereqs.sh
+++ prereqs.sh
@@ -3,16 +3,16 @@
 debian_virtualenv_check() {
   virtualenv -h > /dev/null
   if [ $? -ne 0 ]; then
-    echo "virtualenv3 is missing...installing..."
-    sudo apt update && sudo apt install -y virtualenv python3-virtualenv
+    echo "virtualenv is missing...installing..."
+    sudo apt update && sudo apt install -y python-virtualenv
   fi
 }
 
 debian_pip_check() {
   pip3 -h > /dev/null
   if [ $? -ne 0 ]; then
-    echo "pip3 is missing...installing..."
-    sudo apt update && sudo apt install -y python3-pip
+    echo "pip is missing...installing..."
+    sudo apt update && sudo apt install -y python-pip
   fi
 }
 
